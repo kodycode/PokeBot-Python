adc4758d7a1a23a4c4e3020042e23713aabc4622
kodythach <kodythach@gmail.com>
2020-10-04 21:13:06 -0400

    Updated trainer error msgs

--- cogs/modules/pokemon_functionality.py
+++ cogs/modules/pokemon_functionality.py
@@ -317,7 +317,8 @@ class PokemonFunctionality:
             return False
         elif user_id not in self.trainer_data:
             await ctx.send("Trainer hasn't set off on his journey to "
-                           "catch 'em all yet.")
+                           "catch 'em all yet. (Catch a pokemon first in "
+                           "order to use this bot command.")
             return False
         return True
 
@@ -769,7 +770,8 @@ class PokemonFunctionality:
                     total_pkmn_count += pinventory[pkmn]
             else:
                 await ctx.send("Trainer hasn't set off on his journey to "
-                               "catch 'em all yet.")
+                               "catch 'em all yet. (Catch a pokemon first in "
+                               "order to use this bot command.")
                 return
             em = discord.Embed()
             em.set_author(name=user_obj)
