14f01605f261b907144bbe438621c0c1f861c769
Kody Thach <kodythach@gmail.com>
2023-01-20 23:49:09 -0500

    Updated bot.py structure

--- bot.py
+++ bot.py
@@ -1,69 +1,74 @@
+from bot_logger import logger
 from discord import Intents
 from discord.ext import commands
-from bot_logger import logger
 import json
 import logging
 
-CONFIG_JSON_PATH = "settings/config.json"
+
 COG_MANAGER = "cogs.cog_manager"
-with open(CONFIG_JSON_PATH) as config:
-    config_data = json.load(config)
-intents = Intents(messages=True, message_content=True, dm_messages=True, dm_reactions=True, reactions=True)
+CONFIG_JSON_PATH = "bot_config.json"
+
+
+config_file = open(CONFIG_JSON_PATH)
+config_data = json.load(config_file)
+intents = Intents(
+    messages=True,
+    message_content=True,
+    dm_messages=True,
+    dm_reactions=True,
+    reactions=True
+)
 bot = commands.Bot(command_prefix=config_data["cmd_prefix"],
-                   description="Renedition of PokeBot",
-                   intents=intents)
+    description="Renedition of PokeBot",
+    intents=intents)
 
 
-class PokeBot:
-    """Initiates the Bot"""
-
-    def __init__(self):
-        bot.run(config_data["token"])
-
-    @bot.event
-    async def on_ready():
-        try:
-            await bot.load_extension(COG_MANAGER)
-        except Exception as e:
-            error_msg = 'Failed to load cog manager\n{}: {}'.format(type(e).__name__, e)
-            print(error_msg)
-            logger.error(error_msg)
-
-    @bot.event
-    async def on_message(message):
-        message.content = message.content.lower()
-        if not message.author.bot:
-            await bot.process_commands(message)
-
-    @bot.event
-    async def on_command_error(error, ctx):
-        if isinstance(error, commands.errors.MissingRequiredArgument):
-            await send_cmd_help(ctx)
-        if isinstance(error, commands.errors.BadArgument):
-            await send_cmd_help(ctx)
+@bot.event
+async def on_ready():
+    try:
+        await bot.load_extension(COG_MANAGER)
+    except Exception as e:
+        error_msg = 'Failed to load cog manager\n{}: {}'.format(type(e).__name__, e)
+        print(error_msg)
+        logger.error(error_msg)
 
 
-async def send_cmd_help(ctx):
+@bot.event
+async def on_message(message):
+    message.content = message.content.lower()
+    if not message.author.bot:
+        await bot.process_commands(message)
+
+
+@bot.event
+async def on_command_error(error, ctx):
+    if isinstance(error, commands.errors.MissingRequiredArgument):
+        await _send_cmd_help(ctx)
+    if isinstance(error, commands.errors.BadArgument):
+        await _send_cmd_help(ctx)
+
+
+async def _send_cmd_help(ctx):
     if ctx.invoked_subcommand:
         pages = bot.formatter.format_help_for(ctx, ctx.invoked_subcommand)
         for page in pages:
             await bot.send_message(ctx.message.channel,
-                                   "Please make sure you're entering a valid"
-                                   "command:\n{}".format(page))
+                                "Please make sure you're entering a valid"
+                                "command:\n{}".format(page))
     else:
         pages = bot.formatter.format_help_for(ctx, ctx.command)
         for page in pages:
             await bot.send_message(ctx.message.channel,
-                                   "Command failed. Please make sure you're "
-                                   "entering the correct arguments to the "
-                                   "command:\n{}".format(page))
+                                "Command failed. Please make sure you're "
+                                "entering the correct arguments to the "
+                                "command:\n{}".format(page))
 
 
 def main():
     try:
         logger.info('Starting bot..')
         print("Starting bot..")
-        PokeBot()
+        bot.run(config_data["token"])
     except Exception as e:
         logging.error('Bot failed to run: {}'.format(str(e)))
         print(e)
