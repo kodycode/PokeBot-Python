d55e4c916196d97d81f6b40b40b892fad1c468f8
Kody Thach <kodythach@gmail.com>
2023-01-23 01:13:16 -0500

    Added GeneralRatesDAO

---
+++ database/general_rates.py
@@ -0,0 +1,31 @@
+from classes import ConfigDAO
+
+
+GENERAL_CONFIG_NAME = "general_config.json"
+
+
+class GeneralRatesDAO(ConfigDAO):
+    """
+    Gets the general rate configs for PokeBot
+    """
+    def __init__(self, filename=GENERAL_CONFIG_NAME):
+        super().__init__(filename)
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(GeneralDAO, cls).__new__(cls)
+            return cls.instance
+        return cls.instance
+
+    def get_daily_redemption_reset_hour(self) -> dict:
+        """
+        Gets the hour to reset the daily
+        """
+        return self.data["daily_redemption_reset_hour"]
+
+    def get_catch_cooldown_seconds(self) -> dict:
+        """
+        Gets the list of gifted lootboxes to receive from
+        PokeBot
+        """
+        return self.data["catch_cooldown_seconds"]
