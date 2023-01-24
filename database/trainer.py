7d71713f092392640ab123e9d6672bec3b223ad5
Kody Thach <kodythach@gmail.com>
2023-01-24 04:17:31 -0500

    Changed trainer dao to accomadate all trainers

--- database/trainer.py
+++ database/trainer.py
@@ -14,10 +14,8 @@ class TrainerDAO(DataDAO):
     LOOTBOX = "lootbox"
     DAILY_TOKENS = "daily_tokens"
 
-    def __init__(self, user_id: str, filename=TRAINER_JSON_FILE):
+    def __init__(self, filename=TRAINER_JSON_FILE):
         super().__init__(filename)
-        self.user_id = user_id
-        self.trainer_data = self.data[self.user_id]
 
     def __new__(cls):
         if not hasattr(cls, 'instance'):
@@ -25,64 +23,87 @@ class TrainerDAO(DataDAO):
             return cls.instance
         return cls.instance
 
-    def get_pokemon_inventory(self) -> dict:
+    def is_existing_trainer(self, user_id: str) -> bool:
+        """
+        Checks if trainer exists and returns True if so,
+        otherwise False
+        """
+        return bool(self.data.get(user_id, False))
+
+    def get_pokemon_inventory(self, user_id: str) -> dict:
         """
         Gets the inventory of pokemon from the trainer
         """
-        return self.trainer_data[self.PINVENTORY]
+        return self.data[user_id][self.PINVENTORY]
 
-    def get_last_catch_time(self) -> float:
+    def get_last_catch_time(self, user_id: str) -> float:
         """
         Gets the last catch time of the trainer
         """
-        return self.trainer_data[self.LAST_CATCH_TIME]
+        return self.data[user_id][self.LAST_CATCH_TIME]
 
-    def get_last_daily_redeemed_time(self) -> float:
+    def get_last_daily_redeemed_time(self, user_id: str) -> float:
         """
         Gets the last daily redeemed time of the trainer
         """
-        return self.trainer_data[self.LAST_DAILY_REDEEMED_TIME]
+        return self.data[user_id][self.LAST_DAILY_REDEEMED_TIME]
 
-    def get_lootbox_inventory(self) -> dict:
+    def get_lootbox_inventory(self, user_id: str) -> dict:
         """
         Gets the inventory of lootboxes that the trainer has
         """
-        return self.trainer_data[self.LOOTBOX]
+        return self.data[user_id][self.LOOTBOX]
 
-    def get_daily_tokens(self) -> int:
+    def get_daily_tokens(self, user_id: str) -> int:
         """
         Gets the list of daily tokens that the trainer has
         """
-        return self.trainer_data[self.DAILY_TOKENS]
+        return self.data[user_id][self.DAILY_TOKENS]
 
-    def set_pokemon_inventory(self, pkmn_name: str, quantity: int) -> None:
+    def set_pokemon_inventory(self, user_id: str, pkmn_name: str, quantity: int) -> None:
         """
         Sets the quantity of a specific trainer's pokemon
         within their inventory
         """
-        self.data[self.user_id][self.PINVENTORY][pkmn_name] = quantity
+        self.data[user_id][self.PINVENTORY][pkmn_name] = quantity
 
-    def set_last_catch_time(self, time: float) -> None:
+    def set_last_catch_time(self, user_id: str, time: float) -> None:
         """
         Sets the trainer's last daily redeemed time
         """
-        self.data[self.user_id][self.LAST_CATCH_TIME] = time
+        self.data[user_id][self.LAST_CATCH_TIME] = time
 
-    def set_last_daily_redeemed_time(self, time: float) -> None:
+    def set_last_daily_redeemed_time(self, user_id: str, time: float) -> None:
         """
         Sets the trainer's last daily redeemed time
         """
-        self.data[self.user_id][self.LAST_DAILY_REDEEMED_TIME] = time
+        self.data[user_id][self.LAST_DAILY_REDEEMED_TIME] = time
 
-    def set_lootbox_inventory(self, lootbox_tier_name: str, quantity: int) -> None:
+    def set_lootbox_inventory(self, user_id: str, lootbox_tier_name: str, quantity: int) -> None:
         """
         Sets the quantity of a specific lootbox tier within
         the trainer's inventory
         """
-        self.data[self.user_id][self.LOOTBOX][lootbox_tier_name] = quantity
+        self.data[user_id][self.LOOTBOX][lootbox_tier_name] = quantity
 
-    def set_daily_tokens(self, quantity: int) -> None:
+    def set_daily_tokens(self, user_id: str, quantity: int) -> None:
         """
         Sets the trainer's daily token amount
         """
-        self.data[self.user_id][self.DAILY_TOKENS] = quantity
+        self.data[user_id][self.DAILY_TOKENS] = quantity
+
+    def generate_new_trainer(self, user_id: str) -> None:
+        """
+        Generates a new trainer in the database
+        """
+        self.data[user_id] = {}
+        self.data[user_id][self.PINVENTORY] = {}
+        self.data[user_id][self.LOOTBOX] = {
+            "bronze": 0,
+            "silver": 0,
+            "gold": 0,
+            "legendary": 0,
+        }
+        self.data[user_id][self.LAST_CATCH_TIME] = 0
+        self.data[user_id][self.LAST_DAILY_REDEEMED_TIME] = 0
+        self.data[user_id][self.DAILY_TOKENS] = 0
